7bf911e24165f5cb4dd18c65157f3b7d1e884952
Dennis Mbithi <denmbithi@gmail.com>
2023-11-28 15:23:37 +0300

    chapter 1 exercise solutions

---
+++ chapter_01/exe_03_counting.py
@@ -0,0 +1,5 @@
+print("Getting ready to count .....")
+for x in range(10):
+    print("I'm counting and at ", x)
+
+print("Done counting")
---
+++ chapter_01/exe_04_fruit-code.py
@@ -0,0 +1,2 @@
+fruit = input("Enter your favourite fruit and press enter: ")
+print("Your favourite fruit is", fruit.capitalize())
